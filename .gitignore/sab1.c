9f61a6bdf7541963c9608cac861a78ecf0efa113
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-03-15 16:14:03 +0530

    Create sab1.c

---
+++ .gitignore/sab1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    int i;
+    scanf("%s",&a[i]);
+    for(i=strlen(a);i>=0;i--)
+    {
+        printf("%c",a[i]);
+    }
+    
+
+    return 0;
+}
